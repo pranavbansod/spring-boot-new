47f165f8376001cf58ef80b554f734dbedeb9539
Pranav Bansod <pranavb@thoughtworks.com>
2018-11-14 14:41:20 +0530

    Added tests for helloController

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("controller")
+@ComponentScan("app.controller")
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/app/controller/HelloController.java
+++ src/main/java/app/controller/HelloController.java
@@ -1,4 +1,4 @@
-package controller;
+package app.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
