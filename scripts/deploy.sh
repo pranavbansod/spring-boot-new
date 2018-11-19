689c5d58fb00fed1fdbaf647fee334879fd7fe6b
Pranav Bansod <pranavb@thoughtworks.com>
2018-11-19 22:20:33 +0530

    Temp Build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,8 @@
 brew install heroku/brew/heroku
 heroku create spring-boot-crud-user
 
+git add ./src/main/resources/public
+git commit -m "bundled assets"
+
 git push -f heroku master
 heroku ps:scale web=1
