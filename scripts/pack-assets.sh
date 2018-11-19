3365833b6476f5bb67917c95da5fcf5ed8cb3c50
Pranav Bansod <pranavb@thoughtworks.com>
2018-11-19 14:36:21 +0530

    Script to remove unit assets

--- scripts/pack-assets.sh
+++ scripts/pack-assets.sh
@@ -5,3 +5,5 @@
 cd ./src/main/resources/public/
 npm install
 npm run-script pack-assets
+rm -rf node_modules
+rm -rf src
--- src/main/resources/public/src/view/createUser.js
+++ src/main/resources/public/src/view/createUser.js
@@ -1,11 +1,44 @@
 var m = require('mithril');
 
 
-let createUser = {
-  view:function () {
-    return
+const formState = {
+  id:'',
+  name:'',
+  setId:function (rank) {
+    formState.id = rank
+  },
+  setName:function (name) {
+    formState.name = name;
   }
-}
+};
+
+const createUser = {
+  view:function () {
+    return [
+      m('h3',"Add User"),
+      m("label.label", "Id"),
+      m("input.number.input[type=text][placeholder=Id]",{
+        oninput: m.withAttr('value',formState.setId)
+      }),
+      m("label.label", "Name"),
+      m("input.name.input[type=text][placeholder=Name]",{
+        oninput: m.withAttr('value',formState.setName)
+      }),
+      m("br"),
+      m("button.button[type=submit]",{
+        onclick:function () {
+          return m.request({
+            method: 'post',
+            url: '/user',
+            data: {'id': formState.id, 'name': formState.name}
+          }).then(function () {
+            window.location = '/'
+          })
+        }
+      }, "Save"),
+    ]
+  }
+};
 
 
 module.exports = createUser;
